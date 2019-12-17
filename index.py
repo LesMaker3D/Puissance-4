27b1d1a5afc936a919cb3b17a2cca7fb9a91780f
Pierre <pierremaurer@orange.fr>
2019-12-17 11:30:03 +0100

    Ajout des commentaire-tache

--- index.py
+++ index.py
@@ -2,12 +2,12 @@
 from random import randint
 from turtle import *
 
-
+#Crée une grille vide
 def grille_vide():
     g=[[0]*7]*6
     return g
 
-
+#Affiche la grille
 def affiche(g):
     speed(0)
     for l in range(6):
@@ -28,27 +28,21 @@ def affiche(g):
                 down()
                 rond_jaune()
         print("\n",end="")
-        
+#Place un rond jaune     
 def rond_jaune():
     fillcolor('yellow')
     begin_fill()
     circle(20,360)
     end_fill()
 
+#place un rond rouge 
 def rond_rouge():
     fillcolor('red')
     begin_fill()
     circle(20,360)
     end_fill()
-    
-    
-# test
-# affiche(grille_vide())
-#g=[[0,1,0,1,0,1,0],[1,2,1,2,1,2,1],[0,1,0,1,0,1,0],[1,2,1,2,1,2,1],[0,1,0,1,0,1,0],[1,2,1,2,1,2,1]]
 
-
-
-
+ #Vérifie si le coup peut être jouer dans la colonne   
 def coup_possible(g,c):
     c -= 1
     print(g[5][c])
@@ -56,9 +50,34 @@ def coup_possible(g,c):
         return True
     else:
         return False
+#Faire Jouer
+def jouer(g,j,c):
+    
+#Faire Horiz
+def horiz(g,j,l,c):
+
+#Faire Vert
+def vert(g,j,l,c):
+
+#faire diag haut
+def diag_haut(g,j,l,c):
+
+#Faire diag bas
+
+def diag_bas(g,j,l,c)
+
+#Faire fonction victoire
+
+def victoire(g,j):
+
+#Faire fonction match nul
+
+def math_nul(g,j):
+
+#Faire Programme principale 
 
 
-
+# Variable Test
 g = [[1,3,4,5,4,7], [1,".",".",4,5,4], [1,4,6,1,4,5], [1,".",".",4,5,4], [1,".",".",4,5,4], [1,".",".",4,5,4]]
         
 print(coup_possible(g,1))
